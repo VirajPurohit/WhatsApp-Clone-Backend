af026d1742f536848ed6427a62d613a00817220b
Viraj <purohitvirajk@gmail.com>
2024-09-17 13:15:55 +0530

    First Backend Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origins: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+  socket.on("send-message", ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit("recieve-message", {
+        recipients: newRecipients,
+        sender: id,
+        text,
+      });
+    });
+  });
+});
+
+server.listen(5000, () => {
+  console.log("server running at http://localhost:5000");
+});
